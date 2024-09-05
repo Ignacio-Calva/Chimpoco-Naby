27fc0fe9950b28db1101217a4695296ec9a7b0a5
abyy15 <abriljuanatrinidad@gmail.com>
2024-09-05 15:05:39 -0300

    Update Chimpoco Naby.cbp

--- Chimpoco Naby/funciones.h
+++ Chimpoco Naby/funciones.h
@@ -1,1 +1,14 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+void menuPrincipal ();
+void mostrarMenu ();
+
+void modoAventura();
+void modoVersus ();
+void godMode();
+void estadisticas();
+void manual ();
+
+#endif  FUNCIONES_H_INCLUDED
 #pragma once
